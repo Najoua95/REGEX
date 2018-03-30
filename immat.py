e9d2f96f5da6147a6de28f6f88f27fcd14fcca63
Najoua Abbaci <33322698+Najoua95@users.noreply.github.com>
2018-03-30 16:37:09 +0200

    Update immat.py

--- immat.py
+++ immat.py
@@ -1,15 +1,17 @@
 import re
-# immat = input('Quelle est votre immatriculation ? : ')
-# prog = re.compile(r"^[A-Z]{2}([.-]?[0-9]{3})([.-]?[A-Z]{2})$")
 
-# while True :
-#     if prog.search(immat) is not None :
-#         print('Immatriculation valide !')
-#         break
-#     else :
-#         immat = input ("Saisissez une immatriculation (valide) : ")
+#1/Immatriculation 
+immat = input('Quelle est votre immatriculation ? : ')
+prog = re.compile(r"^[A-Z]{2}([.-]?[0-9]{3})([.-]?[A-Z]{2})$")
 
+while True :
+     if prog.search(immat) is not None :
+         print('Immatriculation valide !')
+         break
+     else :
+         immat = input ("Saisissez une immatriculation (valide) : ")
 
+#2/IPv4
 adresse = input('Quelle est votre adresse IPv4 ? : ')
 while True :
     if (re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", adresse)) :
