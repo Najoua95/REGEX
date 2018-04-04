0f2d72f0602fff8fb97b4066fc7333e99ed57c65
Najoua Abbaci <33322698+Najoua95@users.noreply.github.com>
2018-04-04 16:31:21 +0200

    Update expressions_regulieres_utiles.py

--- expressions_regulieres_utiles.py
+++ expressions_regulieres_utiles.py
@@ -24,7 +24,7 @@ while True :
 
 #entier positif avec taille fixe à 5
 nb = input('Votre nombre positif de 5 chiffres : ')
-prog = re.compile(r"^(\d{5})$")
+prog = re.compile(r"^\d{5}$")
 
 while True :
     if prog.search(nb) is not None :
@@ -36,7 +36,7 @@ while True :
 ########################################################################
 #entier relatif sans limite
 nb = input("Votre nombre relatif (sans limite) : ")
-prog = re.compile(r"^\-\d+$")
+prog = re.compile(r"^-\d+$")
 
 while True :
     if prog.search(nb) is not None :
@@ -48,7 +48,7 @@ while True :
 
 #entier relatif limité entre 1 et 10 chiffres
 nb = input('Votre nombre relatif entre 1 et 10 chiffres: ')
-prog = re.compile(r"^\-(\d{1,10})$")
+prog = re.compile(r"^-\d{1,10}$")
 
 while True :
     if prog.search(nb) is not None :
@@ -60,7 +60,7 @@ while True :
 
 #entier relatif limité à 5 chiffres
 nb = input('Votre nombre relatif de 5 chiffres : ')
-prog = re.compile(r"^\-(\d{5})$")
+prog = re.compile(r"^-\d{5}$")
 
 while True :
     if prog.search(nb) is not None :
@@ -72,7 +72,7 @@ while True :
 #####################################################################################
 #entier positif ou relatif sans limite
 nb = input("Votre nombre positif ou relatif (sans limite) : ")
-prog = re.compile(r"^\-?\d+$")
+prog = re.compile(r"^-?\d+$")
 
 while True :
     if prog.search(nb) is not None :
@@ -83,7 +83,7 @@ while True :
 
 #entier positif ou relatif limité entre 1 et 10 chiffres
 nb = input('Votre nombre positif ou relatif entre 1 et 10 chiffres: ')
-prog = re.compile(r"^\-?(\d{1,10})$")
+prog = re.compile(r"^-?\d{1,10}$")
 
 while True :
     if prog.search(nb) is not None :
@@ -94,7 +94,7 @@ while True :
 
 #entier positif ou relatif taille de 5 fixe
 nb = input('Votre nombre positif ou relatif de 5 chiffres : ')
-prog = re.compile(r"^\-?(\d{5})$")
+prog = re.compile(r"^-?\d{5}$")
 
 while True :
     if prog.search(nb) is not None :
@@ -106,7 +106,7 @@ while True :
 ###############################################################################################
 #un nombre : positif ou négatif avec ou sans des chiffres après la virgule.
 nb = input('Votre nombre positif ou négatif avec ou sans virgule : ')
-prog = re.compile(r"^\-?\d+\,?\d?$")
+prog = re.compile(r"^-?\d+\,?\d?$")
 
 while True :
     if prog.search(nb) is not None :
@@ -117,7 +117,7 @@ while True :
 
 #positif ou négatif avec 2 chiffres après la virgule.
 nb = input('Votre nombre positif ou négatif avec 2 chiffres après la virgule : ')
-prog = re.compile(r"^\-?\d+\,\d{2}$")
+prog = re.compile(r"^-?\d+\,\d{2}$")
 
 while True :
     if prog.search(nb) is not None :
@@ -128,7 +128,7 @@ while True :
 
 #nombre version américiane avec '.'
 nb = input('Votre nombre (version us) : ')
-prog = re.compile(r"^\-?\d+[,|.]\d{2}$")
+prog = re.compile(r"^-?\d+[,|.]\d{2}$")
 
 while True :
     if prog.search(nb) is not None :
@@ -150,7 +150,7 @@ while True :
 
 #un nombre : positif ou négatif avec ou sans des chiffres après la virgule et qui comporte ou non le signe %.
 nb = input('Votre nombre : ')
-prog = re.compile(r"^\-?\d+[,|.]?\d{2}\%?$")
+prog = re.compile(r"^-?\d+[,|.]?\d{2}\%?$")
 
 while True :
     if prog.search(nb) is not None :
@@ -165,22 +165,22 @@ while True :
 #cas 1 : entier positif sans limite = ^\d+$
 #cas 2 : entier positif limite de 10 chiffres = ^\d{1,10}$
 #cas 3 : entier positif à 5 chiffres = ^\d{5}$
-#cas 4 : entier relatif sans limite = ^\-\d+$
-#cas 5 : entier relatif limite de 10 chiffres = ^\-(\d{1,10})$
-#cas 6 : entier relatif à 5 chiffres = ^\-(\d{5})$
-#cas 7 : entier positif ou relatif sans limite = ^\-?\d+$
-#cas 8 : entier positif ou relatif sans limite = ^\-?\d+$
-#cas 9 : entier positif ou relatif limite de 10 chiffres = ^\-?(\d{1,10})$
-#cas 10 : entier positif ou relatif limite de 10 chiffres = ^\-?(\d{1,10})$
-#cas 11 : entier positif ou relatif à 5 chiffres = ^\-?(\d{5})$
-#cas 12 : nombre positif ou négatif avec ou sans chiffres après la virgule = ^\-?\d+\,?\d?$
-#cas 13 : nombre positif ou négatif avec 2 chiffres après la virgules = ^\-?\d+\,\d{2}$
-#cas 14 : nombre version américaine = ^\-?\d+[,|.]\d{2}$
+#cas 4 : entier relatif sans limite = ^-\d+$
+#cas 5 : entier relatif limite de 10 chiffres = ^-(\d{1,10})$
+#cas 6 : entier relatif à 5 chiffres = ^-(\d{5})$
+#cas 7 : entier positif ou relatif sans limite = ^-?\d+$
+#cas 8 : entier positif ou relatif sans limite = ^-?\d+$
+#cas 9 : entier positif ou relatif limite de 10 chiffres = ^-?(\d{1,10})$
+#cas 10 : entier positif ou relatif limite de 10 chiffres = ^-?(\d{1,10})$
+#cas 11 : entier positif ou relatif à 5 chiffres = ^-?(\d{5})$
+#cas 12 : nombre positif ou négatif avec ou sans chiffres après la virgule = ^-?\d+\,?\d?$
+#cas 13 : nombre positif ou négatif avec 2 chiffres après la virgules = ^-?\d+\,\d{2}$
+#cas 14 : nombre version américaine = ^-?\d+[,|.]\d{2}$
 #cas 15 : qui peut ou non comporter le signe % = ^\d+\%?$
-#cas 16 : nombre positif ou négatif avec 2 chiffres après la virgules, avec ou sans % = ^\-?\d+[,|.]?\d{2}\%?$
+#cas 16 : nombre positif ou négatif avec 2 chiffres après la virgules, avec ou sans % = ^-?\d+[,|.]?\d{2}\%?$
 
 nb = input('Votre nombre : ')
 #ici, autre méthode, on utilise un while avec un re.match
-while re.match(r"^\-?\d+[,|.]?\d{2}\%?$", nb) is None : #modifier votre regex en fonction du cas recherché
+while re.match(r"^-?\d+[,|.]?\d{2}\%?$", nb) is None : #modifier votre regex en fonction du cas recherché
     nb = input('Entrez un nombre valide : ')
 print("votre nombre est valide : {}".format(nb))
