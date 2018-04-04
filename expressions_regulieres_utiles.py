4af04d6c6435dd80cf7013eb5bdec6d61b1b7c65
Najoua Abbaci <33322698+Najoua95@users.noreply.github.com>
2018-04-04 16:34:56 +0200

    Update expressions_regulieres_utiles.py

--- expressions_regulieres_utiles.py
+++ expressions_regulieres_utiles.py
@@ -12,7 +12,7 @@ while True :
 
 #entier positif avec une taille limite de 10 chiffres
 nb = input('Votre nombre positif entre 1 et 10 chiffres: ')
-prog = re.compile(r"^(\d{1,10})$")
+prog = re.compile(r"^\d{1,10}$")
 
 while True :
     if prog.search(nb) is not None :
@@ -166,13 +166,13 @@ while True :
 #cas 2 : entier positif limite de 10 chiffres = ^\d{1,10}$
 #cas 3 : entier positif à 5 chiffres = ^\d{5}$
 #cas 4 : entier relatif sans limite = ^-\d+$
-#cas 5 : entier relatif limite de 10 chiffres = ^-(\d{1,10})$
-#cas 6 : entier relatif à 5 chiffres = ^-(\d{5})$
+#cas 5 : entier relatif limite de 10 chiffres = ^-\d{1,10}$
+#cas 6 : entier relatif à 5 chiffres = ^-\d{5}$
 #cas 7 : entier positif ou relatif sans limite = ^-?\d+$
 #cas 8 : entier positif ou relatif sans limite = ^-?\d+$
-#cas 9 : entier positif ou relatif limite de 10 chiffres = ^-?(\d{1,10})$
-#cas 10 : entier positif ou relatif limite de 10 chiffres = ^-?(\d{1,10})$
-#cas 11 : entier positif ou relatif à 5 chiffres = ^-?(\d{5})$
+#cas 9 : entier positif ou relatif limite de 10 chiffres = ^-?\d{1,10}$
+#cas 10 : entier positif ou relatif limite de 10 chiffres = ^-?\d{1,10}$
+#cas 11 : entier positif ou relatif à 5 chiffres = ^-?\d{5}$
 #cas 12 : nombre positif ou négatif avec ou sans chiffres après la virgule = ^-?\d+\,?\d?$
 #cas 13 : nombre positif ou négatif avec 2 chiffres après la virgules = ^-?\d+\,\d{2}$
 #cas 14 : nombre version américaine = ^-?\d+[,|.]\d{2}$
