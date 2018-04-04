630d57234ff3e22372e6eacd3b2550c4bf9ca384
Najoua Abbaci <33322698+Najoua95@users.noreply.github.com>
2018-04-04 15:32:23 +0200

    Update regex_mise_en_situation.py

--- regex_mise_en_situation.py
+++ regex_mise_en_situation.py
@@ -46,5 +46,22 @@ def correct_mail():
  correct_mail()
 
 ###################
+#Ecrire 1script qui vérifie que la chaîne saisie par un User est bien celle d’un email, sinon lui demander de resaisir à nouveau (jusqu'à obtenir un email valide) ?
+def correct_mail():
+    email = input("Insérez une adresse mail valide : ")
+    prog = re.compile(r"^[a-zA-Z0-9-._]+@{1}[a-zA-Z0-9]+\.[a-z]{2,4}$")
+    mdp = re.compile(r"^[a-zA-Z0-9]{4,}$")
+    
+    while True : 
+        if prog.search(email) is not None :
+            print('E-mail valide !')
+            mot_de_passe = input("Votre mot de passe ? ")
+            if mdp.search(mot_de_passe) is not None : 
+                print('mot de passe valide')
+            return email
+        else :
+            email = input ("Insérez une adresse mail valide: ")
+correct_mail()
 
+##################
 
